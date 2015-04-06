6d450f55b55319cc1a5741858d813d46241ae387
CastellGaby <castellgaby.dubon@unitec.edu>
2015-04-06 02:51:50 -0600

    New Conection

--- MiniStackOverflow.DataDeployed/Migrations/201504060836026_Initial.Designer.cs
+++ MiniStackOverflow.DataDeployed/Migrations/201504060836026_Initial.Designer.cs
@@ -13,7 +13,7 @@ namespace MiniStackOverflow.DataDeployed.Migrations
         
         string IMigrationMetadata.Id
         {
-            get { return "201504060731469_Initial"; }
+            get { return "201504060836026_Initial"; }
         }
         
         string IMigrationMetadata.Source
